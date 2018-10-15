772e5460d52c51f2a924d5f2db5da16b4a4afde2
Pranav Kulshrestha <hulkcodexl16@gmail.com>
2018-10-16 02:42:49 +0530

    Added code for a good redux excercise

---
+++ portfolio/src/playground/redux101.js
@@ -0,0 +1,50 @@
+import { createStore } from 'redux';
+import { type } from 'os';
+
+const store = createStore((state = { count: 0}, action) => {
+    switch (action.type) {
+        case 'INCREEMENT':
+            const increementBy = typeof action.increementBy === 'number' ? action.increementBy : 1;
+            return {
+                count: state.count + increementBy
+            };
+        case 'DECREEMENT':
+            const decreementBy = typeof action.decreementBy === 'number' ? action.decreementBy : 1;
+            return {
+                count: state.count - decreementBy
+            }
+        case 'RESET': 
+         return {
+             count: 0
+         }
+         case 'SET':
+         return {
+             count: action.count
+         }
+        default:
+            return state;
+        }
+    });
+
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+})
+
+store.dispatch({
+    type: 'INCREEMENT',
+    increementBy: 5
+});
+store.dispatch({
+    type: 'DECREEMENT'
+});
+store.dispatch({
+    type: 'DECREEMENT',
+    decreementBy: 10
+});
+store.dispatch({
+    type: 'RESET'
+});
+store.dispatch({
+    type:'SET',
+    count: 101
+})
